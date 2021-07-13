753789350699946d35c41335cf1a4e78980290aa
Joanna Brigham <joannabrigham@hotmail.co.uk>
2021-07-13 13:53:29 +0200

    Passing test for returning middle 2 letters of even length word

--- jasmine-standalone-3.6.0/src/MiddleLetter.js
+++ jasmine-standalone-3.6.0/src/MiddleLetter.js
@@ -1,8 +1,14 @@
 function middleLetter(word) {
   const wordLength = word.length;
 
+  const oddMiddleLetter = (wordLength - 1) / 2;
+
+  const evenMiddleLetter = wordLength / 2;
+
   if (wordLength % 2 != 0) {
-    return word[(wordLength - 1) / 2];
+    return word[oddMiddleLetter];
+  } else if (wordLength % 2 === 0 && wordLength > 2) {
+    return word.substring(evenMiddleLetter - 1, evenMiddleLetter + 1);
   } else {
     return word;
   }
