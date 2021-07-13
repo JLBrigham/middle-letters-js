a194780ccc319a8a23e64938ab316008e50b8cc6
Joanna Brigham <joannabrigham@hotmail.co.uk>
2021-07-13 13:30:38 +0200

    Passing test for returning middle letter of odd length word

--- jasmine-standalone-3.6.0/src/MiddleLetter.js
+++ jasmine-standalone-3.6.0/src/MiddleLetter.js
@@ -1,3 +1,9 @@
 function middleLetter(word) {
-  return word;
+  const wordLength = word.length;
+
+  if (wordLength % 2 != 0) {
+    return word[(wordLength - 1) / 2];
+  } else {
+    return word;
+  }
 }
