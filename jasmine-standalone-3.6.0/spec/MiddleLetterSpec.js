357af0bb0fe1f157e2f05a0e689aed8a0655cbe0
Joanna Brigham <joannabrigham@hotmail.co.uk>
2021-07-13 13:11:50 +0200

    Failing test for returning full word of 2 letter words

--- jasmine-standalone-3.6.0/spec/MiddleLetterSpec.js
+++ jasmine-standalone-3.6.0/spec/MiddleLetterSpec.js
@@ -7,4 +7,8 @@ describe("middleLetter", function () {
   it("returns the whole word if word is only 1 letter long", function () {
     expect(middleLetter("A")).toEqual("A");
   });
+
+  it("returns thw whole word if word is 2 letters long", function () {
+    expect(middleLetter("of")).toEqual("of");
+  });
 });
